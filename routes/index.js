3c21d9a5e8eb02b95e056e7f8a4cd89daa544c64
Jason Kidd <jason_kidd@live.com.au>
2022-07-10 14:09:59 +1000

    Remove unused users page

--- routes/index.js
+++ routes/index.js
@@ -1,74 +1,16 @@
-var express = require("express");
-var router = express.Router();
-const axios = require("axios");
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
 
-let defaultLocation = {
-  latitude: -27.47,
-  longitude: 153.02,
-  image: 'http://openweathermap.org/img/wn/01d.png',
-  text: "Please enter a city to get started.",
-  week: [{
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-    {
-      summary: "Waiting...",
-      temperatureMin: "Waiting...",
-      temperatureMax: "Waiting..."
-    },
-  ]
-};
-
-let weeklyWeather = [];
+const weeklyWeather = [];
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", {
-    weather: defaultLocation.text,
-    image: defaultLocation.image,
-    cityName: "Let's Begin",
-    weatherTemperature: "Enter A City",
-    weatherDescription: "To Find The Weather",
-    weekly: defaultLocation.week,
-    lat: defaultLocation.latitude,
-    lon: defaultLocation.longitude
-  });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-
     let image,
       name,
       icon,
@@ -79,59 +21,26 @@ router.post("/", async (req, res) => {
       longitude,
       data,
       weatherText,
-      airData
+      airData;
 
-    let city = req.body.city;
+    const { city } = req.body;
 
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`;
 
     // Get data from the OpenWeatherMap API using the city as the query parameter
-    await axios
-      .get(weatherUrl)
-      .then(result => {
-        /* Get the latitude and longitude from the OpenWeatherMap API and store it
-          in the latitude and longitude variables */
-        latitude = result.data.coord.lat;
-        longitude = result.data.coord.lon;
-        name = result.data.name;
-        icon = result.data.weather[0].icon;
-        image = `http://openweathermap.org/img/wn/${icon}.png`;
-        temperature = Math.floor(result.data.main.temp);
-
-        /* Use the latitude and longitude from before to get
-           the Air Quality Reading from the OpenAQ API */
-        let openAirUrl = `https://api.openaq.org/v1/latest?coordinates=${latitude},${longitude}`;
-        axios.get(openAirUrl).then(response => {
-          if (response.data.results.length === 0 || response.data === undefined) {
-            airData = "No Air Quality Available";
-            airDescription = "with no air quality reading available";
-          } else {
-            data = response.data.results[0].measurements[0];
-            airData = `${data.value}${data.unit}`;
-            airDescription = `with an air quality reading of ${data.value}${data.unit}`;
-          }
-        })
-
-        /* Get more weather data from the Dark Sky API
-           using the latitude and longitude from the OpenWeatherMap API
-           as the query parameters */
-        let darkSkyUrl = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latitude},${longitude}?units=ca`;
-        return axios.get(darkSkyUrl)
-          .then(result => {
-            weeklyWeather = [];
-            description = result.data.daily.summary;
-            summary = result.data.hourly.summary;
-            result.data.daily.data.forEach(item => {
-              item.temperatureMin = "MIN: " + Math.floor(item.temperatureMin);
-              item.temperatureMax = "MAX: " + Math.floor(item.temperatureMax);
-              weeklyWeather.push(item);
-            })
-          })
-      })
+    await axios.get(weatherUrl).then((result) => {
+      /* Get the latitude and longitude from the OpenWeatherMap API */
+      latitude = result.data.coord.lat;
+      longitude = result.data.coord.lon;
+      name = result.data.name;
+      icon = result.data.weather[0].icon;
+      image = `http://openweathermap.org/img/wn/${icon}.png`;
+      temperature = Math.floor(result.data.main.temp);
+    });
     /* Render the HTML on the page including the text for the marker used for
       the leaflet map. */
-    weatherText = `The weather in ${name} is ${description} Current temperature is ${temperature} degrees ${airDescription}`
-    res.render("index", {
+    weatherText = `The weather in ${name} is ${description} Current temperature is ${temperature} degrees ${airDescription}`;
+    res.render('index', {
       weather: weatherText,
       cityName: name,
       image: image,
@@ -142,20 +51,19 @@ router.post("/", async (req, res) => {
       lat: latitude,
       lon: longitude,
     });
-
   } catch (error) {
     console.error(error);
     /* Render the error HTML if the city entered by the user
        doesn't contain any data for any of the APIs */
-    return res.render("index", {
-      weather: "Oh no, Something went wrong! Please try again",
+    return res.render('index', {
+      weather: 'Oh no, Something went wrong! Please try again',
       image: defaultLocation.image,
-      cityName: "An Error Occured",
-      weatherTemperature: ":",
-      weatherDescription: "Please Try Again",
+      cityName: 'An Error Occured',
+      weatherTemperature: ':',
+      weatherDescription: 'Please Try Again',
       weekly: defaultLocation.week,
       lat: defaultLocation.latitude,
-      lon: defaultLocation.longitude
+      lon: defaultLocation.longitude,
     });
   }
 });
