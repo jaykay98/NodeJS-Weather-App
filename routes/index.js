a3d651d22876dbc22c62fd25e4c50103017bbe83
Jason Kidd <jason_kidd@live.com.au>
2022-07-11 20:06:03 +1000

    Remove duplicate code

--- routes/index.js
+++ routes/index.js
@@ -2,68 +2,56 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-const weeklyWeather = [];
+const { MAPBOX_ACCESS_TOKEN } = process.env;
 
-/* GET home page. */
+const DEFAULT_LOCATION = {
+  lat: -27.4679,
+  lon: 153.0281,
+  markerDescription: 'Please enter a city to get started...',
+};
+
 router.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('index', {
+    lat: DEFAULT_LOCATION.lat,
+    lon: DEFAULT_LOCATION.lon,
+    MAPBOX_ACCESS_TOKEN,
+    image: DEFAULT_LOCATION.image,
+    markerDescription: DEFAULT_LOCATION.markerDescription,
+  });
 });
 
 router.post('/', async (req, res) => {
   try {
-    let image,
-      name,
-      icon,
-      temperature,
-      airDescription,
-      summary,
-      latitude,
-      longitude,
-      data,
-      weatherText,
-      airData;
-
     const { city } = req.body;
 
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`;
 
-    // Get data from the OpenWeatherMap API using the city as the query parameter
-    await axios.get(weatherUrl).then((result) => {
-      /* Get the latitude and longitude from the OpenWeatherMap API */
-      latitude = result.data.coord.lat;
-      longitude = result.data.coord.lon;
-      name = result.data.name;
-      icon = result.data.weather[0].icon;
-      image = `http://openweathermap.org/img/wn/${icon}.png`;
-      temperature = Math.floor(result.data.main.temp);
-    });
-    /* Render the HTML on the page including the text for the marker used for
-      the leaflet map. */
-    weatherText = `The weather in ${name} is ${description} Current temperature is ${temperature} degrees ${airDescription}`;
-    res.render('index', {
-      weather: weatherText,
-      cityName: name,
-      image: image,
-      weatherTemperature: temperature,
-      weatherDescription: summary,
-      airReading: airData,
-      weekly: weeklyWeather,
-      lat: latitude,
-      lon: longitude,
+    await axios.get(weatherUrl).then((results) => {
+      const { name } = results.data;
+      const { lat, lon } = results.data.coord;
+      const { icon, description, main } = results.data.weather[0];
+      const { feels_like } = results.data.main;
+      const image = `http://openweathermap.org/img/wn/${icon}.png`;
+      const temperature = `${Math.floor(results.data.main.temp)}${'\u2103'}`;
+      const markerDescription = `The weather in ${name} is ${description}. It feels like ${feels_like} degrees celsius`;
+      res.render('index', {
+        lat,
+        lon,
+        image,
+        MAPBOX_ACCESS_TOKEN,
+        markerDescription,
+        temperature,
+        cityName: name,
+        weatherConditions: main,
+      });
     });
   } catch (error) {
-    console.error(error);
-    /* Render the error HTML if the city entered by the user
-       doesn't contain any data for any of the APIs */
+    DEFAULT_LOCATION.markerDescription = 'City not found! Please try again.';
     return res.render('index', {
-      weather: 'Oh no, Something went wrong! Please try again',
-      image: defaultLocation.image,
-      cityName: 'An Error Occured',
-      weatherTemperature: ':',
-      weatherDescription: 'Please Try Again',
-      weekly: defaultLocation.week,
-      lat: defaultLocation.latitude,
-      lon: defaultLocation.longitude,
+      lat: DEFAULT_LOCATION.lat,
+      lon: DEFAULT_LOCATION.lon,
+      MAPBOX_ACCESS_TOKEN,
+      markerDescription: DEFAULT_LOCATION.markerDescription,
     });
   }
 });
